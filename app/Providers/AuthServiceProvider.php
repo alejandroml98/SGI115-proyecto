f13e47c96c2df265f2349d5e40d596aa8a810d0a
alejandro Martinez <alejandro2ml22@gmail.com>
2021-06-01 03:41:09 +0000

    Registro de usuarios y verificación de correo por Email

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,13 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        //                        
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verificar correo electrónico')
+                ->line('Por favor confirma tu correo electrónico.')
+                ->line('Para ello simplemente debes hacer click en el siguiente enlace:')
+                ->action('Clic para confirmar su correo electrónico', $url);
+        });
     }
 }
