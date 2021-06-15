a945dc7604ccaf8a5f98f15565d15f026344f754
alejandro Martinez <alejandro2ml22@gmail.com>
2021-06-15 11:00:58 -0600

    traduccion de validaciones a español

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Tu contraseña ha sido reiniciada!',
+    'sent' => 'Hemos enviado a tu correo el link de reinicio de contraseña!',
+    'throttled' => 'Por favor esperar antes de volver a intentar.',
+    'token' => 'Este token de reinicio de contraseña es invalido.',
+    'user' => "No podemos encontrar un usuario con este correo electronico.",
 
 ];
